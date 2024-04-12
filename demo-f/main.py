873f27833130eb94972f24ed500461a65eb3ff4c
Marisol C <147443547+MarisolCor@users.noreply.github.com>
2024-04-11 18:25:26 -0700

    Update main.py

--- demo-f/main.py
+++ demo-f/main.py
@@ -1,17 +1,25 @@
 from flask import Flask, send_from_directory, request, jsonify
 from server import CrimePredictor
-
+import logging
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 app = Flask(__name__, static_url_path='/')
 app.static_folder = 'dist'
 
 
 @app.route('/')
 def root():
+    logging.info('serving the home page!')  # will not print anything
     return send_from_directory('dist', 'index.html')
+
+@app.route('/health')
+def health():
+    logging.info('health check is called now~')  # will not print anything
+    return "OK"
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    logging.info('serving the predictor')  # will not print anything
     try:
         data = request.json
         csv = data.get('csv')
@@ -22,4 +30,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    logging.info('=== STARTING CRIME RATE APPLICATION===')  # will not print anything
+    app.run(host='0.0.0.0', port=8000, debug=False)
